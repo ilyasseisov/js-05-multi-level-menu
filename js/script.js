91e44e46afa6d3e26f8a87ca63bd2f37feb6d8c4
Ilyas Seisov <ilyasseisov@gmail.com>
2022-09-29 04:55:58 -0700

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Variables
 const mainMenu = document.querySelector('.main-menu');
 const settingsMenu = document.querySelector('.settings-menu');
 const helpMenu = document.querySelector('.help-menu');
@@ -7,34 +8,50 @@ const helpSupportBtn = document.querySelector('#help-support');
 const backBtnFromSettings = document.querySelector('#back-btn-from-settings');
 const backBtnFromHelp = document.querySelector('#back-btn-from-help');
 
-settingsPrivacyBtn.addEventListener('click', () => {
-  mainMenu.classList.remove('show');
-  settingsMenu.classList.add('show', 'slideLeftToZero');
-  setTimeout(() => {
-    settingsMenu.classList.remove('slideLeftToZero');
-  }, 500);
-});
-helpSupportBtn.addEventListener('click', () => {
-  mainMenu.classList.remove('show');
-  helpMenu.classList.add('show', 'slideLeftToZero');
-  setTimeout(() => {
-    helpMenu.classList.remove('slideLeftToZero');
-  }, 500);
-});
+// Functions
 
-backBtnFromSettings.addEventListener('click', () => {
-  settingsMenu.classList.remove('show');
-  mainMenu.classList.add('show', 'slideRightToZero');
-
+const addClass = (className, element) => {
+  element.classList.add(className);
+};
+const removeClass = (className, element) => {
+  element.classList.remove(className);
+};
+const removeClassDelayed = (className, element, delay) => {
   setTimeout(() => {
-    mainMenu.classList.remove('slideRightToZero');
-  }, 500);
-});
-backBtnFromHelp.addEventListener('click', () => {
-  helpMenu.classList.remove('show');
-  mainMenu.classList.add('show', 'slideRightToZero');
+    removeClass(className, element);
+  }, delay);
+};
 
-  setTimeout(() => {
-    mainMenu.classList.remove('slideRightToZero');
-  }, 500);
-});
+const goToSettings = () => {
+  removeClass('show', mainMenu);
+  addClass('show', settingsMenu);
+  addClass('slideLeftToZero', settingsMenu);
+  removeClassDelayed('slideLeftToZero', settingsMenu, 500);
+};
+
+const goToHelp = () => {
+  removeClass('show', mainMenu);
+  addClass('show', helpMenu);
+  addClass('slideLeftToZero', helpMenu);
+  removeClassDelayed('slideLeftToZero', helpMenu, 500);
+};
+
+const goFromSettings = () => {
+  removeClass('show', settingsMenu);
+  addClass('show', mainMenu);
+  addClass('slideRightToZero', mainMenu);
+  removeClassDelayed('slideRightToZero', mainMenu, 500);
+};
+
+const goFromHelp = () => {
+  removeClass('show', helpMenu);
+  addClass('show', mainMenu);
+  addClass('slideRightToZero', mainMenu);
+  removeClassDelayed('slideRightToZero', mainMenu, 500);
+};
+
+// Event Listeners
+settingsPrivacyBtn.addEventListener('click', goToSettings);
+helpSupportBtn.addEventListener('click', goToHelp);
+backBtnFromSettings.addEventListener('click', goFromSettings);
+backBtnFromHelp.addEventListener('click', goFromHelp);
